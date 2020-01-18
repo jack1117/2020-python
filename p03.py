9f6800bf17589213cd216f1cae78255af4422867
jack1117 <60027100+jack1117@users.noreply.github.com>
2020-01-18 14:05:48 +0800

    Add files via upload

---
+++ p03.py
@@ -0,0 +1,6 @@
+a=12.34
+print(a)
+print(type(a))
+
+b=123.123456789
+print(b)
---
+++ p08.py
@@ -0,0 +1,11 @@
+a=input('請輸入職級:')
+
+if a=='A' or a=='B':
+    print('年終6個月')
+elif a in ['C','D']:
+    print('年終4個月')
+elif a in ['E','F']:
+    print('年終3個月')
+else:
+    print('年終2個月')
+    
